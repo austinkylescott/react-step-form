c8e39d268d9a78b1aeec63d063a831ed2dce5a65
Austin Scott <austin.scott18@gmail.com>
2019-06-23 11:05:43 -0400

    completed 3 step form

---
+++ src/components/Success.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+
+export class success extends Component {
+  continue = e => {
+    e.preventDefault();
+    //PROCESS FORM//
+    this.props.nextStep();
+  };
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Success" />
+          <h1>Thank you for your submission</h1>
+          <p>You will receive an email with further instructions.</p>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+
+export default success;
